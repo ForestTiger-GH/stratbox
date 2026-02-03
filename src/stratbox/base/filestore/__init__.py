485445b8696b6b9fb26ae2cdc696b95a0b05cc98
ForestTiger-GH <merkulov.dmit@gmail.com>
2026-02-03 21:41:19 +0300

    Add temp catalog processing

--- src/stratbox/base/filestore/__init__.py
+++ src/stratbox/base/filestore/__init__.py
@@ -1,5 +1,6 @@
 from stratbox.base.filestore.base import FileStore
 from stratbox.base.filestore.local import LocalFileStore
 from stratbox.base.filestore.types import FileStat
+from stratbox.base.filestore.tmp import make_workdir, workdir
 
-__all__ = ["FileStore", "LocalFileStore", "FileStat"]
+__all__ = ["FileStore", "LocalFileStore", "FileStat", "make_workdir", "workdir"]
